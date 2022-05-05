1ea2caae968bbaf1228286863f45493cd6e187f1
Julianne Swinoga <julianneswinoga@gmail.com>
2022-05-05 15:40:40 -0400

    trx_to_excel.py working

--- trx_to_excel.py
+++ trx_to_excel.py
@@ -7,12 +7,13 @@ import xlsxwriter
 from colorhash import ColorHash
 from colour import Color
 
-from parser import parse_tracex_buffer
+from file_parser import parse_tracex_buffer
 from events import *
 
-parser = argparse.ArgumentParser(description="""aaaaa""")
-parser.add_argument('input_csvs', nargs='+', action='store',
-                    help='Path to the input csv file(s) that contains TraceX event data (exported from Tracealyzer)')
+parser = argparse.ArgumentParser(description="""
+Convert TraceX event dumps into an Excel spreadsheet""")
+parser.add_argument('input_trxs', nargs='+', action='store',
+                    help='Path to the input trx file(s) that contains TraceX event data')
 
 sem_ptr_map = {
     0x1018B318: 'blockingRxCompleted',
@@ -31,65 +32,29 @@ timeout_map = {
 remap_rtn_type = List[Union[str, Dict]]
 
 
-class uartOpen(TraceXEvent):
-    fn_name = 'uartOpen'
-    arg_map = ['line_num', '_2', '_3', '_4']
-
-
-class uartClose(TraceXEvent):
-    fn_name = 'uartClose'
-    arg_map = ['line_num', CommonArgsMap.obj_id, '_3', '_4']
-
-
-class uartRead(TraceXEvent):
-    fn_name = 'uartRead'
-    arg_map = [CommonArgsMap.obj_id, 'count', 'vmin', 'vtime']
-
-
-class uartReadBufferBlockingPl(TraceXEvent):
-    fn_name = 'uartReadBufferBlockingPl'
-    arg_map = ['xferlen', CommonArgsMap.timeout, '_3', '_4']
-
-
-class uartWaitForReceiveToCompleteWithBuffer(TraceXEvent):
-    fn_name = 'uartWaitForReceiveToCompleteWithBuffer'
-    arg_map = ['bytesReq', CommonArgsMap.timeout, '_3', '_4']
-
-
-class uartWaitForReceiveToCompleteWithBufferReturnEarly(TraceXEvent):
-    fn_name = 'uartWaitForReceiveToCompleteWithBufferReturnEarly'
-
-
-class uartWaitForReceiveToCompleteWithBufferReturnLate(TraceXEvent):
-    fn_name = 'uartWaitForReceiveToCompleteWithBufferReturnLate'
-
-
-class uartReadMinimumBytesBlocking(TraceXEvent):
-    fn_name = 'uartReadMinimumBytesBlocking'
-    arg_map = ['xferlen', 'vmin', 'vtime', '_4']
-
-
-class uartISRError(TraceXEvent):
-    fn_name = 'uartISRError'
-    arg_map = ['notifyLevel', 'cond1', 'cond2', '_4']
-
-
-class uartSetRxFifoTriggerLevelPl(TraceXEvent):
-    fn_name = 'uartSetRxFifoTriggerLevelPl'
-    arg_map = ['triglevel', '_3', '_4', '_4']
-
-
 custom_events_map = {
-    5000: uartOpen,
-    5001: uartClose,
-    5002: uartRead,
-    5003: uartReadBufferBlockingPl,
-    5004: uartWaitForReceiveToCompleteWithBuffer,
-    5005: uartWaitForReceiveToCompleteWithBufferReturnEarly,
-    5006: uartWaitForReceiveToCompleteWithBufferReturnLate,
-    5007: uartReadMinimumBytesBlocking,
-    5008: uartISRError,
-    5009: uartSetRxFifoTriggerLevelPl,
+    5000: tracex_event_factory('uartOpen', class_name_is_fn_name=True,
+                               arg_map=['line_num', '_2', '_3', '_4']),
+    5001: tracex_event_factory('uartClose', class_name_is_fn_name=True,
+                               arg_map=['line_num', CommonArg.obj_id, '_3', '_4']),
+    5002: tracex_event_factory('uartRead', class_name_is_fn_name=True,
+                               arg_map=[CommonArg.obj_id, 'count', 'vmin', 'vtime']),
+    5003: tracex_event_factory('uartReadBufferBlockingPl', class_name_is_fn_name=True,
+                               arg_map=['xferlen', CommonArg.timeout, '_3', '_4']),
+    5004: tracex_event_factory('uartWaitForReceiveToCompleteWithBuffer',class_name_is_fn_name=True,
+                               arg_map=['bytesReq', CommonArg.timeout, '_3', '_4']),
+    5005: tracex_event_factory('uartWaitForReceiveToCompleteWithBufferReturnEarly', class_name_is_fn_name=True,
+                               arg_map=['_1', '_2', '_3', '_4']),
+    5006: tracex_event_factory('uartWaitForReceiveToCompleteWithBufferReturnLate', class_name_is_fn_name=True,
+                               arg_map=['_1', '_2', '_3', '_4']),
+    5007: tracex_event_factory('uartReadMinimumBytesBlocking', class_name_is_fn_name=True,
+                               arg_map=['xferlen', 'vmin', 'vtime', '_4']),
+    5008: tracex_event_factory('uartISRError', class_name_is_fn_name=True,
+                               arg_map=['notifyLevel', 'cond1', 'cond2', '_4']),
+    5009: tracex_event_factory('uartSetRxFifoTriggerLevelPl', class_name_is_fn_name=True,
+                               arg_map=['triglevel', '_2', '_3', '_4']),
+    5010: tracex_event_factory('ioctl', class_name_is_fn_name=True,
+                               arg_map=[CommonArg.obj_id, 'num', '_3', '_4']),
 }
 
 
@@ -100,7 +65,7 @@ meta_row_type = List[Union[int, str, Union[str, rich_event_cell], Dict]]
 
 
 def convert_file(filepath: str) -> List[converted_row_type]:
-    events = parse_tracex_buffer(filepath, custom_events_map)
+    events, reg_map = parse_tracex_buffer(filepath, custom_events_map)
 
     out_lines = [['time', 'actor', 'event']]  # header
     for event in events:
@@ -108,33 +73,25 @@ def convert_file(filepath: str) -> List[converted_row_type]:
         thread_format = {'font_color': ColorHash(thread_str).hex}
 
         fn_str = str(event.id if event.fn_name is None else event.fn_name)
-        if event.id > 4096:
-            fn = [{'bold': True}, fn_str, '(']
-        else:
-            fn = [fn_str, '(']
+        fn = [{'bold': True}, fn_str] if event.id > 4096 else [fn_str]
 
-        if isinstance(event.args, list):
-            if event.arg_map is None:
-                arg_list = [','.join(f'{arg_num}={hex(arg_val)}' for arg_num, arg_val in enumerate(event.args))]
-            else:
-                arg_list = [','.join(f'{arg_name}={hex(arg_val)}' for arg_name, arg_val in zip(event.arg_map, event.args))]
-        else:
-            arg_list = []
-            for arg_name, arg_val in event.mapped_args.items():
-                if arg_name == CommonArgsMap.obj_id and arg_val in sem_ptr_map:
-                    arg_val = sem_ptr_map[arg_val]
+        arg_list = []
+        for arg_name, arg_val in event.mapped_args.items():
+            if arg_name == CommonArg.obj_id and arg_val in sem_ptr_map:
+                arg_val = sem_ptr_map[arg_val]
 
-                if arg_name in [CommonArgsMap.obj_id]:
-                    arg_format = {'font_color': ColorHash(arg_name).hex}
-                    arg_list.append(arg_format)
-                arg_list.append(f'{arg_name}={arg_val},')
+            if arg_name in [CommonArg.obj_id]:
+                arg_format = {'font_color': ColorHash(arg_val).hex}
+                arg_list.append(arg_format)
+            arg_list.append(f'{arg_name}={arg_val},')
+        # arg_list = [','.join(f'{arg_name}={hex(arg_val)}' for arg_name, arg_val in zip(event.arg_map, event.args))]
 
-        event_str = fn + arg_list + [')']
+        event_str = fn + ['('] + arg_list + [')']
         out_lines.append([event.timestamp, [thread_format, thread_str], event_str])
     return out_lines
 
 
-meta_match_type = Optional[Tuple[str, int, int]]
+meta_match_type = Optional[Tuple[str, str, int, int]]
 
 
 class MetaMatches:
@@ -142,49 +99,50 @@ class MetaMatches:
     def critical_section(lines, i) -> meta_match_type:
         if i + 5 > len(lines) - 1:
             return None
-        semget = SemGetEvent.fn_name in str(lines[i + 0][2]) and 'txBufferLock' in str(lines[i + 0][2])
-        tid1 = ThreadIdEvent.fn_name in str(lines[i + 1][2])
-        preempt1 = ThreadPreemptionChangeEvent.fn_name in str(lines[i + 2][2])
-        tid2 = ThreadIdEvent.fn_name in str(lines[i + 3][2])
-        preempt2 = ThreadPreemptionChangeEvent.fn_name in str(lines[i + 4][2])
-        semput = SemPutEvent.fn_name in str(lines[i + 5][2]) and 'txBufferLock' in str(lines[i + 5][2])
+        semget = event_id_map[83].fn_name in str(lines[i + 0][2]) and 'txBufferLock' in str(lines[i + 0][2])
+        tid1 = event_id_map[103].fn_name in str(lines[i + 1][2])
+        preempt1 = event_id_map[107].fn_name in str(lines[i + 2][2])
+        tid2 = event_id_map[103].fn_name in str(lines[i + 3][2])
+        preempt2 = event_id_map[107].fn_name in str(lines[i + 4][2])
+        semput = event_id_map[80].fn_name in str(lines[i + 5][2]) and 'txBufferLock' in str(lines[i + 5][2])
         if semget and tid1 and preempt1 and tid2 and preempt2 and semput:
-            return 'green', i, i + 5
+            return 'criticalSection', 'green', i, i + 5
         return None
 
     @staticmethod
     def rx_transfer(lines, i) -> meta_match_type:
-        semget = SemGetEvent.fn_name in str(lines[i + 0][2]) and 'rxTransferLock' in str(lines[i + 0][2])
+        semget = event_id_map[83].fn_name in str(lines[i + 0][2]) and 'rxTransferLock' in str(lines[i + 0][2])
         if not semget:
             return None
         for offset in range(len(lines) - i):
-            semput = SemPutEvent.fn_name in str(lines[i + offset][2]) and 'rxTransferLock' in str(lines[i + offset][2])
+            semput = event_id_map[80].fn_name in str(lines[i + offset][2]) and 'rxTransferLock' in str(lines[i + offset][2])
             if semput:
-                return 'yellow', i, i + offset
+                return 'rxTransfer', 'yellow', i, i + offset
         return None
 
     @staticmethod
     def mutex_locks(lines, i) -> meta_match_type:
         # we can always assume that mutexes are used for locking
-        mtxget = MtxGetEvent.fn_name in str(lines[i + 0][2])
+        mtxget = event_id_map[52].fn_name in str(lines[i + 0][2])
         if not mtxget:
             return None
-        mtxName = lines[i + 0][2][2].split(CommonArgsMap.obj_id, maxsplit=1)[1].split(',', maxsplit=1)[0]
+        # Good god convert this to a regex
+        mtxName = lines[i + 0][2][3].split(CommonArg.obj_id, maxsplit=1)[1].split(',', maxsplit=1)[0].split('=', maxsplit=1)[1]
         for offset in range(len(lines) - i):
-            mtxPut = MtxPutEvent.fn_name in str(lines[i + offset][2]) and mtxName in str(lines[i + offset][2])
+            mtxPut = event_id_map[57].fn_name in str(lines[i + offset][2]) and mtxName in str(lines[i + offset][2])
             if mtxPut:
-                return 'purple', i, i + offset
+                return mtxName, 'purple', i, i + offset
         return None
 
     @staticmethod
     def rx_completed(lines, i) -> meta_match_type:
-        semput = SemPutEvent.fn_name in str(lines[i + 0][2]) and 'blockingRxCompleted' in str(lines[i + 0][2])
+        semput = event_id_map[80].fn_name in str(lines[i + 0][2]) and 'blockingRxCompleted' in str(lines[i + 0][2])
         if not semput:
             return None
         for offset in range(len(lines) - i):
-            semget = SemGetEvent.fn_name in str(lines[i + offset][2]) and 'blockingRxCompleted' in str(lines[i + offset][2])
+            semget = event_id_map[83].fn_name in str(lines[i + offset][2]) and 'blockingRxCompleted' in str(lines[i + offset][2])
             if semget:
-                return 'red', i, i + offset
+                return 'blockingRxCompleted', 'red', i, i + offset
         return None
 
 
@@ -204,7 +162,7 @@ def add_trace_metadata(lines: List[converted_row_type]) -> List[meta_row_type]:
 
     meta_lines = lines
     for meta_index, meta_tuple in enumerate(meta_tuples):
-        meta_color, meta_start, meta_end = meta_tuple
+        meta_name, meta_color, meta_start, meta_end = meta_tuple
         meta_color = Color(meta_color)  # Convert to Color class
         # toggle the hue back and forth to split up meta sections that are right next to each other
         hue_offset = -0.03 if meta_index % 2 == 0 else 0.03
@@ -213,7 +171,7 @@ def add_trace_metadata(lines: List[converted_row_type]) -> List[meta_row_type]:
                               meta_color.luminance)).hex_l
         for i in range(meta_start, meta_end + 1):
             meta_dict = {
-                'value': meta_index,
+                'value': f'{meta_name}|{meta_index}',
                 'formatting': {'bg_color': bg_color},
             }
             meta_lines[i].append(meta_dict)
@@ -263,7 +221,7 @@ def write_workbook(filepath: str, lines: List[meta_row_type]):
 
 
 def main():
-    for input_filepath in args.input_csvs:
+    for input_filepath in args.input_trxs:
         converted_lines = convert_file(input_filepath)
         metadata_lines = add_trace_metadata(converted_lines)
         if '.' in input_filepath:
