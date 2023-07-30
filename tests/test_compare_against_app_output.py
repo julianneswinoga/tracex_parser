e6b1e5627a811325ea87fc4e2a2a45a0acbec2dd
Julianne Swinoga <julianneswinoga@gmail.com>
2023-07-30 10:49:50 -0400

    Convert raw test loop into pytest parameters

--- tests/test_compare_against_app_output.py
+++ tests/test_compare_against_app_output.py
@@ -1,22 +1,27 @@
+import pytest
+from typing import Tuple
+
 from parse_app_output import parse_app_txt_output
 
 from tracex_parser.file_parser import parse_tracex_buffer
 
+filename_tups = [
+    ('demo_threadx.trx', 'tests/demo_threadx.txt'),
+]
 
-def test_against_app_output():
-    demo_to_txt_output = {
-        'demo_threadx.trx': 'tests/demo_threadx.txt',
-    }
 
-    for trx_filename, txt_filename in demo_to_txt_output.items():
-        events, obj_map = parse_tracex_buffer(f'./{trx_filename}')
-        app_output_data = parse_app_txt_output(txt_filename)
-        assert len(app_output_data.event_list) == len(events)
-        for app_event, parsed_event in zip(app_output_data.event_list, events):
-            assert app_event.time_stamp == parsed_event.timestamp
-            assert app_event.event_type == parsed_event.id
-            assert app_event.cur_context_ptr == parsed_event.thread_ptr
-            # Txt output sometimes doesn't have the full name? "System Timer Thr"
-            assert app_event.cur_context_name in parsed_event.thread_name
-            assert app_event.event_infos == parsed_event.raw_args
-            assert app_event.priority == parsed_event.thread_priority
+@pytest.mark.parametrize('filename_tup', filename_tups)
+def test_against_app_output(filename_tup: Tuple[str, str]):
+    trx_filename, txt_filename = filename_tup
+
+    events, obj_map = parse_tracex_buffer(f'./{trx_filename}')
+    app_output_data = parse_app_txt_output(txt_filename)
+    assert len(app_output_data.event_list) == len(events)
+    for app_event, parsed_event in zip(app_output_data.event_list, events):
+        assert app_event.time_stamp == parsed_event.timestamp
+        assert app_event.event_type == parsed_event.id
+        assert app_event.cur_context_ptr == parsed_event.thread_ptr
+        # Txt output sometimes doesn't have the full name? "System Timer Thr"
+        assert app_event.cur_context_name in parsed_event.thread_name
+        assert app_event.event_infos == parsed_event.raw_args
+        assert app_event.priority == parsed_event.thread_priority
--- tests/test_full_integration.py
+++ tests/test_full_integration.py
@@ -1,4 +1,5 @@
-from typing import NamedTuple
+import pytest
+from typing import NamedTuple, Tuple
 
 from tracex_parser.file_parser import parse_tracex_buffer
 
@@ -10,24 +11,27 @@ class TrxDemoStats(NamedTuple):
     num_unique_events: int
 
 
-def test_demo_trx_files():
-    demo_stats = {
-        'demo_filex.trx': TrxDemoStats(950, 949000, 7, 25),
-        'demo_netx_tcp.trx': TrxDemoStats(950, 949000, 19, 36),
-        'demo_netx_udp.trx': TrxDemoStats(950, 949000, 17, 23),
-        'demo_threadx.trx': TrxDemoStats(974, 40402, 16, 11),
-    }
+filename_stat_tups = [
+    ('demo_filex.trx', TrxDemoStats(950, 949000, 7, 25)),
+    ('demo_netx_tcp.trx', TrxDemoStats(950, 949000, 19, 36)),
+    ('demo_netx_udp.trx', TrxDemoStats(950, 949000, 17, 23)),
+    ('demo_threadx.trx', TrxDemoStats(974, 40402, 16, 11)),
+]
 
-    for trx_filename, tested_stats in demo_stats.items():
-        events, obj_map = parse_tracex_buffer(f'./{trx_filename}')
 
-        assert len(events) == tested_stats.total_events
+@pytest.mark.parametrize('filename_stat_tup', filename_stat_tups)
+def test_demo_trx_files(filename_stat_tup: Tuple[str, TrxDemoStats]):
+    trx_filename, tested_stats = filename_stat_tup
 
-        total_ticks = events[-1].timestamp - events[0].timestamp
-        assert total_ticks == tested_stats.total_ticks
+    events, obj_map = parse_tracex_buffer(f'./{trx_filename}')
 
-        obj_reg_size = len(obj_map.keys())
-        assert obj_reg_size == tested_stats.obj_registry_size
+    assert len(events) == tested_stats.total_events
 
-        unique_events = set(e.id for e in events)
-        assert len(unique_events) == tested_stats.num_unique_events
+    total_ticks = events[-1].timestamp - events[0].timestamp
+    assert total_ticks == tested_stats.total_ticks
+
+    obj_reg_size = len(obj_map.keys())
+    assert obj_reg_size == tested_stats.obj_registry_size
+
+    unique_events = set(e.id for e in events)
+    assert len(unique_events) == tested_stats.num_unique_events
